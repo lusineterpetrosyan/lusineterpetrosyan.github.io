a6e60c12d6985b5004c46eef44bdfd6306cd0ced
Lusine Ter-Petrosyan <lusine.terpetrosyan@gmail.com>
2014-05-22 13:29:37 +0400

    Changing

--- public/javascripts/directive.js
+++ public/javascripts/directive.js
@@ -126,7 +126,7 @@ var myDirectives = angular.module('my-directives',[]);
 	myDirectives.directive('bigMenu', function(){
 		return{
 			restrict: 'E',
-			templateUrl: '../partials/_about.html'
+			templateUrl: '../partials/_menu.html'
 		};
 	});
 })();
