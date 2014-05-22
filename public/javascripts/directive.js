268801fc10cf8e16990035c2bf3f43d8cb1edc37
Lusine Ter-Petrosyan <lusine.terpetrosyan@gmail.com>
2014-05-22 13:18:20 +0400

    Changing

--- public/javascripts/directive.js
+++ public/javascripts/directive.js
@@ -126,7 +126,7 @@ var myDirectives = angular.module('my-directives',[]);
 	myDirectives.directive('bigMenu', function(){
 		return{
 			restrict: 'E',
-			templateUrl: 'https://github.com/lusineterpetrosyan/lusineterpetrosyan.github.io/blob/master/partials/_menu.html'
+			templateUrl: '../partials/_menu.html'
 		};
 	});
 })();
